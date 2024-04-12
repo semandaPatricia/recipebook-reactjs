2ad71259319529f113256583530dbcfff412d48d
semanda <patash248@gmail.com>
2024-04-12 22:34:46 +0200

    added embla carousel autoplay

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,24 @@ import Home from "./pages/home/index";
 import About  from "./pages/about/index";
 import  Blog  from "./pages/blog/index";
 import  Contact  from "./pages/contact/index";
-import ActiveSlider from "./components/ActiveSlider";
-import AquaticAnimals from "./components/Example"
+//import ActiveSlider from "./components/ActiveSlider";
+//import AquaticAnimals from "./components/Example"
+import EmblaCarousel from './components/EmblaCarousel'
 
+import './styles/base.css'
+import './styles/embla.module.css'
+import './styles/embla.css'
+
+const OPTIONS = { loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 function App() {
   return (
     <>
       <Router>
         <NavBar />
         
-
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
